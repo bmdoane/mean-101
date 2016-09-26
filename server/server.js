678665842f9596c82e5c37b49e5182f3e41dd42e
Brian Doane <brianmdoane@gmail.com>
2016-09-26 12:24:59 -0500

    sending message to db from inputs

--- server/server.js
+++ server/server.js
@@ -3,17 +3,66 @@
 // Express executes in the context of where your node modules directory
 // MEAN app - NO res.render
 const express = require('express')
+const mongoose = require('mongoose')
+const { json } = require('body-parser')
 
 const app = express()
-const port = process.env.PORT || 3000
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/meanchat'
+const PORT = process.env.PORT || 3000
 
+// Middleware
+// MEAN app - No url encoded forms, only json
 app.use(express.static('client'))
+app.use(json())
 
 // Route
 // When someone make a request to /api/title return title
 app.get('/api/title', (req, res) =>
 	// Always send an object - json based on res.send
-	res.json({ title: 'MEAN 101 from Node' })
+	res.json({ title: 'MEAN Chat' })
 )
 
-app.listen(port, () => console.log(`Listening on port: ${port}`))
+const Message = mongoose.model('message', {
+	author: 'String',
+	content: 'String',
+})
+
+app.get('/api/messages', (req, res, err) =>
+  Message
+    .find()
+    .then(messages => res.json({ messages }))
+    .catch(err)
+)
+
+// Most backend code is going to be db queries
+app.post('/api/messages', (req, res, err) => {
+  const msg = req.body
+  Message
+    .create(msg)
+    .then(msg => res.json(msg))
+    .catch(err)
+})
+
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URL, () =>
+  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+)
+
+// app.get('/api/messages', (req, res) =>
+//   res.json({
+//     messages: [
+//       {
+//         author: 'Tiberious',
+//         content: 'You suck Spock',
+//       },
+//       {
+//         author: 'Spock',
+//         content: 'That is highly illogical'
+//       },
+//       {
+//         author: 'Red Shirt',
+//         content: 'Scotty is dead'
+//       }
+//     ]
+//   })
+// )
