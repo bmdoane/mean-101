7b378828fd645a360f4b04e36e21d8984be4661c
Brian Doane <brianmdoane@gmail.com>
2016-09-26 10:56:33 -0500

    Get title from node

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 'use strict'
 // minimale server
 // Express executes in the context of where your node modules directory
+// MEAN app - NO res.render
 const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-//app.set('port', port)
-
 app.use(express.static('client'))
+
+// Route
+// When someone make a request to /api/title return title
+app.get('/api/title', (req, res) =>
+	// Always send an object - json based on res.send
+	res.json({ title: 'MEAN 101 from Node' })
+)
 
 app.listen(port, () => console.log(`Listening on port: ${port}`))
